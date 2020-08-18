17cb3977129ca5f726e5b9df5cac875238d08a80
Alexander Zhuravlev <alex15023@yandex.ru>
2020-08-18 10:35:30 +0300

    Починил докер

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM python:alpine3.8
+FROM python:latest
 COPY . /app
 WORKDIR /app
 RUN pip install --user -r requirements.txt
 EXPOSE 8080
-CMD [ "python", "manage.py", "runserver", "8080" ]
+CMD [ "python", "manage.py", "runserver", "0.0.0.0:8080" ]
--- image_resizer/settings.py
+++ image_resizer/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'wqy4cegdl&z6s2#bk4zq0^yan2y**plhp7&lm+gp)!ys^vvp57'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
